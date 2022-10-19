71a0ffccce5fa6fd7849507a816d3f79fa9d0301
aman7896 <amansagar7896@gmail.com>
2022-10-19 21:00:43 +0530

    added input validation

--- src/main/java/com/Ordina/CodeChallenge/RestController/WordFrequencyController.java
+++ src/main/java/com/Ordina/CodeChallenge/RestController/WordFrequencyController.java
@@ -3,6 +3,8 @@ package com.Ordina.CodeChallenge.RestController;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +18,9 @@ import com.Ordina.CodeChallenge.service.WordFrequencyService;
 
 @RestController
 public class WordFrequencyController {
+	
+	
+	Logger logger = LoggerFactory.getLogger(WordFrequencyController.class);
 	
 	@Autowired
 	private WordFrequencyService service;
@@ -32,11 +37,17 @@ public class WordFrequencyController {
 	
 	@PostMapping("/getWordFrequency") //This service will accept JSON input in the format {"text":"<Some text>","word":"<word to be searched>"}
 	public int calculateFrequencyForWord (@RequestBody Map<String, String> request) {
-		return service.calculateFrequencyForWord(request.get("text"), request.get("word"));
+		if(request.containsKey("text") && request.containsKey("word"))
+			return service.calculateFrequencyForWord(request.get("text"), request.get("word"));
+		logger.info("Invalid input for calculateFrequencyForWord");
+		return 0;
 	}
 	
 	@PostMapping("/getNFreqWords") //This service will accept JSON input in the format {"text":"<Some text>","number":"<number of records>"}
 	public List<WordFrequency> calculateMostFrequentNWords (@RequestBody Map<String, String> request){
-		return service.calculateMostFrequentNWords(request.get("text"), Integer.parseInt(request.get("number")));
+		if(request.containsKey("text") && request.containsKey("number"))
+			return service.calculateMostFrequentNWords(request.get("text"), Integer.parseInt(request.get("number")));
+		logger.info("Invalid input for calculateMostFrequentNWords");
+		return null;
 	}
 }
